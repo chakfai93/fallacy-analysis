f63d0724ce57e6a5dfc758a28d63515afa857807
chin-brainrot <154074427+chakfai93@users.noreply.github.com>
2026-03-26 20:35:27 +0800

    Create background/service-worker.js with Service Worker code

---
+++ background/service-worker.js
@@ -0,0 +1,13 @@
+// Service Worker example
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing...');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating...');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Fetch event for', event.request.url);
+});
