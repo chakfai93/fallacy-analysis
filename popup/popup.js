6ebb0617167bcc03b502f8773440dcddd41d651b
chin-brainrot <154074427+chakfai93@users.noreply.github.com>
2026-03-26 20:35:41 +0800

    Add main popup controller in popup.js

---
+++ popup/popup.js
@@ -0,0 +1,13 @@
+// Main Popup Controller
+function PopupController() {
+    this.show = function() {
+        // Code to show the popup
+    };
+    this.hide = function() {
+        // Code to hide the popup
+    };
+}
+
+// Usage
+const popup = new PopupController();
+popup.show();
